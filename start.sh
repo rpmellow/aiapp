b44f61ffb0eb9d065df17fda30d5bcefd694cd85
rpmellow <rp.mellow.escapade@gmail.com>
2025-08-02 10:16:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@ sudo apt install python3-pip
 sudo rm /usr/lib/python3.12/EXTERNALLY-MANAGED 
 pip3 install -r requirements.txt 
 curl -fsSL https://ollama.com/install.sh | sh
-ollama pull llama3.2:1b
 pip install ollama
 sudo apt install uvicorn
+ollama pull llama3.2:1b
+ollama pull qwen2.5:1.5b
 uvicorn main:app --host 0.0.0.0 --port 8080
